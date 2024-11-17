b33965f63f794e7380e64a13a5f96c84007e2a1e
Aryansh Tripathi <tripathi.aryansh.2002@gmail.com>
2024-11-17 15:54:18 +0530

    first commit

---
+++ src/middleware/validation.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import { body, validationResult } from "express-validator";
+
+const handleValidationErrors = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		return res.status(400).json("errors: errors.array()");
+	}
+	next();
+};
+
+export const validateMyUserRequest = [
+	body("name")
+		.isString()
+		.notEmpty()
+		.withMessage("Name must be a finite string"),
+	body("addressLine1")
+		.isString()
+		.notEmpty()
+		.withMessage("Address Line 1 must be a finite string"),
+	body("city")
+		.isString()
+		.notEmpty()
+		.withMessage("City must be a finite string"),
+	body("country")
+		.isString()
+		.notEmpty()
+		.withMessage("Country must be a finite string"),
+	handleValidationErrors,
+];
